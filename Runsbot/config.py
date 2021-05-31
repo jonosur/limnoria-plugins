0cb4ae4bb654671532442ab7c2115a49330696dc
grateful <36494074+jonosur@users.noreply.github.com>
2021-05-31 16:49:39 -0400

    Update config.py

--- Runsbot/config.py
+++ Runsbot/config.py
@@ -28,7 +28,6 @@ Runsbot = conf.registerPlugin('Runsbot')
 # This is where your configuration variables (if any) should go.  For example:
 conf.registerGlobalValue(Runsbot, 'checkInterval', registry.NonNegativeInteger(5, """Positive Integer in seconds to check."""))
 conf.registerGlobalValue(Runsbot, 'delayInterval', registry.NonNegativeInteger(30, """Positive Integer in seconds for delay."""))
-conf.registerGlobalValue(Runsbot, 'dateFlip', registry.NonNegativeInteger(400, """Positive Integer which represents time 0400 means 4am"""))
 conf.registerGlobalValue(Runsbot, 'isFirst', registry.Boolean(True, """Used to force outputs on reload, debug tool"""))
 conf.registerGlobalValue(Runsbot, 'isLoaded', registry.Boolean(False, """Announce Basesloaded"""))
 conf.registerGlobalValue(Runsbot, 'isDebug', registry.Boolean(False, """Debug"""))
