4ac56c497882f600c6fe2e2241af42d890c98030
grateful <36494074+jonosur@users.noreply.github.com>
2023-02-15 04:25:50 -0500

    Update __init__.py

--- RotoNews/__init__.py
+++ RotoNews/__init__.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2017, Jonathan "grateful" Surman
+# Copyright (c) 2017, grateful
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
